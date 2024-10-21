fe6d4d1fee45f02804b6c03205ef5150ea970994
Ger Hobbelt <ger@hobbelt.com>
2024-10-21 08:01:50 +0200

    fix compiler errors in monolithic build mode.

--- src/marl_bench.cpp
+++ src/marl_bench.cpp
@@ -16,7 +16,7 @@
 
 #include "marl/sanitizers.h"
 
-int main(int argc, char** argv) {
+int main(int argc, const char** argv) {
 #if MARL_ADDRESS_SANITIZER_ENABLED
   printf(
       "***WARNING*** Marl built with address sanitizer enabled. "
